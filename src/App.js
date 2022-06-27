f6a5595ab6a2b4cc056eca57da976cdc2e2aa1f2
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-27 14:16:31 +0530

    added try and catch block for possible erorr

--- src/App.js
+++ src/App.js
@@ -4,17 +4,48 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [moviesData, setMoviesData] = useState([]);
+  const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
- async function fetchMoviesHandler() {
-  setIsLoading(true)
-  const response  = await fetch('https://swapi.dev/api/films');
-    
-    const data = await response.json()
-      console.log(data)
-      setMoviesData(data.results)
-      setIsLoading(false)
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  };
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -22,10 +53,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && <MoviesList movies={moviesData} />}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -6,15 +6,15 @@ import classes from './MoviesList.module.css';
 const MovieList = (props) => {
   return (
     <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
-        <Movie
-          key={movie.episode_id}
-          title={movie.title}
-          releaseDate={movie.release_date}
-          openingText={movie.opening_crawl}
-        />
-      ))}
-    </ul>
+    {props.movies.map((movie) => (
+      <Movie
+        key={movie.id}
+        title={movie.title}
+        releaseDate={movie.releaseDate}
+        openingText={movie.openingText}
+      />
+    ))}
+  </ul>
   );
 };
 
