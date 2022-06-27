bcfe3a0de4071ae007b380add5f74e1da028550a
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-27 22:21:32 +0530

    added POST methode using fetch, also using firebase database to store data

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -6,15 +6,15 @@ import classes from './MoviesList.module.css';
 const MovieList = (props) => {
   return (
     <ul className={classes['movies-list']}>
-    {props.movies.map((movie) => (
-      <Movie
-        key={movie.id}
-        title={movie.title}
-        releaseDate={movie.releaseDate}
-        openingText={movie.openingText}
-      />
-    ))}
-  </ul>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.release}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
   );
 };
 
